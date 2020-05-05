3f4c75be4705d877bb9b9d631ca59a3073c98c5b
Depressia <cray86@mail.ru>
2020-05-05 12:46:53 +0300

    Корректирует хедер на всех страницах + добавляет js для прозрачности в закрытом состоянии.

--- source/js/menu-toggle.js
+++ source/js/menu-toggle.js
@@ -1,14 +1,21 @@
+var header = document.querySelector(".page-header");
+
 var navMain = document.querySelector(".main-nav");
 var navToggle = document.querySelector(".main-nav__toggle");
 
 navMain.classList.remove("main-nav--nojs");
+header.classList.add("page-header--menu-hide");
 
 navToggle.addEventListener("click", function() {
   if (navMain.classList.contains("main-nav--closed")) {
     navMain.classList.remove("main-nav--closed");
-     navMain.classList.add("main-nav--opened");
+    navMain.classList.add("main-nav--opened");
+    header.classList.remove("page-header--menu-hide");
+    header.classList.add("page-header--menu-show");
   } else {
     navMain.classList.add("main-nav--closed");
     navMain.classList.remove("main-nav--opened");
+    header.classList.add("page-header--menu-hide");
+    header.classList.remove("page-header--menu-show");
   }
 })
